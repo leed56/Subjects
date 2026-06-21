a964c4147b71ea6e0ca0850d04a3c48eae783d81
leed56 <38248206+leed56@users.noreply.github.com>
2026-06-21 14:48:19 +0530

    Update screen UI

--- lakbiz/app/next.config.ts
+++ lakbiz/app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_ADMIN_ONLY: process.env.NEXT_PUBLIC_ADMIN_ONLY ?? "true",
+  },
 };
 
 export default nextConfig;
